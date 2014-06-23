14764b0292d73ff9961b90feebd22d21e1dc6889
Svetoslav Mishkov <svetoslav.mishkov@gmail.com>
2014-06-23 17:35:55 +0300

    Added tests for reports.

---
+++ tests_reports.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once("./GAPI.class.php");
+
+class Test_PHP_API extends PHPUnit_Framework_TestCase {
+
+    protected $ok_user = 'tester2';
+    protected $ok_pass = '123456789';
+
+    protected function setUp() {
+        $this->api = new GAPI($this->ok_user, $this->ok_pass);
+
+        // A report is set up on the tester2 user by running the report generator
+        // with default options.
+        $this->api->reports_listing();
+        $this->report_id = $this->api->result[0]['id'];
+    }
+
+    public function test__reports_listing() {
+        // WARNING: The old API returns error if there are no reports:
+        // 500: <class 'getanewsletter.reports.models.DoesNotExist'>:Report matching query does not exist.
+        // This behaviour will not be tested (nor simulated) in the new API.
+
+        $result = $this->api->reports_listing();
+        $this->assertTrue($result);
+        $this->assertEquals(count($this->api->result), 1);
+
+        // WARNING: The old tool returns a key with typo: 'unsubsribe' instead of 'unsubscribe':
+        $keys = Array('lists', 'date' , 'sent_to', 'unsubsribe', 'unique_opens', 'url', 'bounces', 'id', 'link_click',
+                      'subject', 'opens');
+
+        foreach($keys as $key) {
+            $this->assertTrue(array_key_exists($key, $this->api->result[0]), 'Missing key: ' . $key);
+        }
+    }
+
+    public function test__reports_bounces() {
+        // List all bounces:
+        $result = $this->api->reports_bounces($this->report_id);
+        $this->assertTrue($result);
+        $this->assertEquals(count($this->api->result), 30);
+        foreach(Array('status', 'email') as $key) {
+            $this->assertTrue(array_key_exists($key, $this->api->result[0]), 'Missing key: ' . $key);
+        }
+
+        // List some bounces:
+        $result = $this->api->reports_bounces($this->report_id, null, 10, 20);
+        $this->assertTrue($result);
+        $this->assertEquals(count($this->api->result), 10);
+
+        // Filter bounces by type. Soft:
+        $result = $this->api->reports_bounces($this->report_id, 'soft');
+        $this->assertTrue($result);
+        $this->assertEquals(count($this->api->result), 24);
+
+        // Filter bounces by type. Hard:
+        $result = $this->api->reports_bounces($this->report_id, 'hard');
+        $this->assertTrue($result);
+        $this->assertEquals(count($this->api->result), 6);
+
+        // Combine filter with limit:
+        $result = $this->api->reports_bounces($this->report_id, 'soft', 10, 20);
+        $this->assertTrue($result);
+        $this->assertEquals(count($this->api->result), 10);
+    }
+}
+
+?>
